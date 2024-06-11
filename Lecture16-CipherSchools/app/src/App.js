314134581ca6984139cae0e6ff245fea95acc709
Japnoor Singh <sjapnoor11@gmail.com>
2024-06-11 12:38:48 +0530

    Lecture 16 completed

---
+++ Lecture16-CipherSchools/app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  const NAME = "Japnoor singh"
+  return (
+    <div>
+      <h1 style={{
+        color : "red",
+        backgroundColor : "cyan",
+        textAlign : "center"
+      }}>Hello world</h1>
+      <h1>Hello from {NAME}</h1>
+    </div>
+  )
+}
+
+export default App

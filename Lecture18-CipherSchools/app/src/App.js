7f55db580da3b6661ac67c379cb8ae280551b536
Japnoor Singh <sjapnoor11@gmail.com>
2024-06-12 13:36:06 +0530

    Lecture 18 completed

---
+++ Lecture18-CipherSchools/app/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import ToDoScreen from "./screens/ToDoScreens";
+
+class App extends Component{
+  render(){
+    return(
+      <ToDoScreen/>
+    )
+  }
+}
---
+++ Lecture18-CipherSchools/app/src/screens/ToDoScreens.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class ToDoScreen extends Component{
+    render(){
+        return(
+            <h1 className="ui heading center">Welcome to TODO List app</h1>
+        )
+    }
+}
+
+export default ToDoScreen;

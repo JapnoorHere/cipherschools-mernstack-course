3657feef58b21900a55462b70133cf468716a9ae
Japnoor Singh <sjapnoor11@gmail.com>
2024-07-01 14:47:48 +0530

    Lecture 44 completed

---
+++ Lecture43-CipherSchools/routes/user-routes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router();
+const userController = require("../controllers/user-controller");
+
+
+router.post("/login", userController.loginUser);
+router.post("/signup", userController.addNewUser);
+
+module.exports = router;
